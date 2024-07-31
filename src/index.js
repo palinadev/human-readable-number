6cdb7e87e7c2fe417b885f98730d5d07fbc7495a
royalehoneybadger <takethejess@gmail.com>
2024-07-31 11:21:03 +0200

    feat: function realization

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function toReadable (number) {
-  
+    const readable = require('humanize-plus');
+    const readableNum = humanize.compactInteger(number);
+    return readableNum
 }
